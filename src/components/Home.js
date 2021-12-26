13f626b6dd9243cb228daa98fcb49730da30bf84
reza khani <reza.khani1998@gmail.com>
2021-12-27 00:12:12 +0330

    Apollo Client

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,17 @@
-const Home = ()=>{
+import { gql, useQuery } from '@apollo/client';
+const GET_MOVIES = gql`
+	query {
+		movies(limit: 30, rating: 5) {
+			id
+			medium_cover_image
+		}
+	}
+`;
 
-	return <h1>Home</h1>
-}
+const Home = () => {
+	const { loading, error, data } = useQuery(GET_MOVIES);
+	console.log(data);
+	return <h1>Home 🚀</h1>;
+};
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { client } from './apollo';
 
 import App from './components/App';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
 	</React.StrictMode>,
 	document.getElementById('root'),
 );
