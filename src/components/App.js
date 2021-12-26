62a6599acdd18588fa7a1affb4948cd96135b561
reza khani <reza.khani1998@gmail.com>
2021-12-26 22:13:05 +0330

    Router and Reset Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,16 @@
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import Detail from './Detail';
+import Home from './Home';
+
 function App() {
-	return <div className="App">reza</div>;
+	return (
+		<Router>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/:id" element={<Detail />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
